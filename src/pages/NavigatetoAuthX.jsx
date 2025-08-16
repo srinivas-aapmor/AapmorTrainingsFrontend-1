8d0fa5107a832ed8e52be87338ada53e343379b7
srinivas-aapmor <ysrinivas@aapmor.com>
2025-08-16 11:29:45 +0530

    Update NavigatetoAuthX.jsx

--- src/pages/NavigatetoAuthX.jsx
+++ src/pages/NavigatetoAuthX.jsx
@@ -3,10 +3,8 @@ import Loader from "../helpers/Loader";
 
 export default function NavigatetoAuthX() {
   useEffect(() => {
-    // Use only the base URL if that's what AuthX has registered
     const redirectUri = encodeURIComponent(import.meta.env.VITE_REDIRECT_URL);
-
-    const domain = new URL(import.meta.env.VITE_DOMAIN).hostname;
+    const domain = import.meta.env.VITE_DOMAIN;
 
     const authXUrl = `${import.meta.env.VITE_AUTHX_URL}?redirect_uri=${redirectUri}&domain=${domain}`;
     window.location.href = authXUrl;
